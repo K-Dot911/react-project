f5a124db36032309f719d05a7ef8f38435a1a189
sagalatov sergey <sagalatov@mail.ru>
2020-11-16 22:46:28 +0300

    added part of tests

--- src/example.test.ts
+++
@@ -1,7 +0,0 @@
-//describe обертка для тестовых блоков.
-
-describe('Math.max', () => {
-    test('should return 2 when add one and one', () => {
-        expect(1 + 1).toBe(2)
-    })
-})
---
+++ src/shared/Dropdown/__test__/Dropdown.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Dropdown} from "../Dropdown";
+import {shallow} from "enzyme";
+
+
+
+describe('Dropdown', function () {
+    test('should render', () => {
+        const wrapper = shallow(<Dropdown children={<div/>} button={<button/>}/>)
+        expect(wrapper).toBeDefined();
+        expect(wrapper.find('#button')).toBeDefined();
+    })
+
+    test('should render snapshot', () => {
+        const wrapper = shallow(<Dropdown children={<div/>} button={<button/>}/>)
+        expect(wrapper).toMatchSnapshot()
+    })
+
+
+});
