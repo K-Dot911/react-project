75b0d96a5ba7c58a0b20497dad450f5ee090d46c
sagalatov sergey <sagalatov@mail.ru>
2020-12-08 22:43:09 +0300

    get token

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { hot } from 'react-hot-loader/root';
 import {Layout} from "./shared/Layout";
 import './main.global.css'
 import {Header} from "./shared/Header";
 import {Content} from "./shared/Content";
 import {CardsList} from "./shared/CardsList";
-import {Text} from "./shared/Text";
+import {useToken} from "./hooks/useToken";
 
 function AppComponent() {
+    const [token] = useToken();
     return (
        <Layout>
-           <Header />
+           <Header token={token}/>
            <Content>
               <CardsList />
               <br/>
--- src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
+++ src/shared/Header/SearchBlock/UserBlock/UserBlock.tsx
@@ -10,7 +10,7 @@ interface IUserBlockProps {
 
 export function UserBlock({avatarSrc, username}: IUserBlockProps) {
     return (
-        <a href="https://www.reddit.com/api/v1/authorize?client_id=5hqULEkloJUlSA&response_type=code&state=random_string&redirect_uri=http://localhost:3000/auth&duration=permanent&scope=read submit identity" className={styles.userBox}>
+        <a href="https://www.reddit.com/api/v1/authorize?client_id=RE-tQhIXZLOrbw&response_type=code&state=random_string&redirect_uri=http://localhost:3000/auth&duration=permanent&scope=read submit identity" className={styles.userBox}>
             <div className={styles.avatarBox}>
                 {avatarSrc ?
                     <img src={avatarSrc} alt="user avatar" className={styles.avatarImage}/>
