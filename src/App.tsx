3a25cd5c0025270f52d1617e3f42eb4f0677e75c
Sagalatov Sergey <sagalatov@mail.ru>
2020-12-16 21:19:43 +0300

    add context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import {Layout} from "./shared/Layout";
 import './main.global.css'
@@ -6,17 +6,23 @@ import {Header} from "./shared/Header";
 import {Content} from "./shared/Content";
 import {CardsList} from "./shared/CardsList";
 import {useToken} from "./hooks/useToken";
+import {tokenContext} from "./shared/context/tokenContext"
 
 function AppComponent() {
+
+    const {Provider} = tokenContext;
     const [token] = useToken();
+
     return (
+        <Provider value={token}>
        <Layout>
-           <Header token={token}/>
+           <Header/>
            <Content>
               <CardsList />
               <br/>
            </Content>
        </Layout>
+        </Provider>
 
     );
 }
