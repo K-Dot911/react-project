e48c8362c7f56dbd186a5ad63d264d7baa921814
Sagalatov Sergey <sagalatov@mail.ru>
2020-12-18 19:10:19 +0300

    Added userContext for data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { hot } from 'react-hot-loader/root';
+import {hot} from 'react-hot-loader/root';
 import {Layout} from "./shared/Layout";
 import './main.global.css'
 import {Header} from "./shared/Header";
@@ -7,22 +7,23 @@ import {Content} from "./shared/Content";
 import {CardsList} from "./shared/CardsList";
 import {useToken} from "./hooks/useToken";
 import {tokenContext} from "./shared/context/tokenContext"
+import {userContext} from "./shared/context/userContext";
+import {UserContextProvider} from "./shared/context/userContext";
 
 function AppComponent() {
-
-    const {Provider} = tokenContext;
     const [token] = useToken();
 
     return (
-        <Provider value={token}>
-       <Layout>
-           <Header/>
-           <Content>
-              <CardsList />
-              <br/>
-           </Content>
-       </Layout>
-        </Provider>
+        <tokenContext.Provider value={token}>
+            <UserContextProvider>
+                <Layout>
+                    <Header/>
+                    <Content>
+                        <CardsList/>
+                    </Content>
+                </Layout>
+            </UserContextProvider>
+        </tokenContext.Provider>
 
     );
 }
